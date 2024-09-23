9a41959a4733c06c24ff076f4420aacab7be1e3d
Congglee <congldqn888@gmail.com>
2024-09-23 14:08:43 +0700

    Updating .env.example

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -8,11 +8,12 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const domain = process.env.NEXT_PUBLIC_APP_URL;
+const fromAddress = process.env.EMAIL_FROM_ADDRESS as string;
 
 export async function sendVerificationEmail(email: string, token: string) {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
   await resend.emails.send({
-    from: "'Budget Tracker' <no-reply@conggglee-dev.com>",
+    from: fromAddress,
     to: email,
     subject: "Confirm your email",
     html: VERIFY_EMAIL_TEMPLATE.replace(
@@ -25,7 +26,7 @@ export async function sendVerificationEmail(email: string, token: string) {
 export async function sendPasswordResetEmail(email: string, token: string) {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
   await resend.emails.send({
-    from: "'Budget Tracker' <no-reply@conggglee-dev.com>",
+    from: fromAddress,
     to: email,
     subject: "Reset your password",
     html: RESET_PASSWORD_EMAIL_TEMPLATE.replace(
@@ -37,7 +38,7 @@ export async function sendPasswordResetEmail(email: string, token: string) {
 
 export async function sendTwoFactorTokenEmail(email: string, token: string) {
   await resend.emails.send({
-    from: "'Budget Tracker' <no-reply@conggglee-dev.com>",
+    from: fromAddress,
     to: email,
     subject: "2FA code",
     html: TWO_FACTOR_AUTHENTICATION_EMAIL_TEMPLATE.replace(
